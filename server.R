0f46c8d469a944dd21b85df8679e9ecce178ad20
Daniel Raduta <datudar@gmail.com>
2018-07-24 12:53:14 -0500

    Update server.R

--- server.R
+++ server.R
@@ -1,20 +1,12 @@
 library(leaflet)
 
 function(input, output) {
-
-  lunch_spots = data.frame(name = c("Aloha Poke", "Chipotle", "Hannah's Bretzel", "Jersey Mike's"),
-                           lng  = c(-87.635703, -87.640880, -87.641357, -87.634020),
-                           lat  = c(41.881920, 41.880406, 41.880538, 41.881261))
+  
+  lunch_spots <- read.csv("lunch_spots.csv", sep = ",", header = TRUE)
   
   observeEvent(input$goButton, {
 
-    idx <- sample(dim(lunch_spots)[1], 1)
-        
-    # output$text <- renderText({
-    #   
-    #   paste0(lunch_spots$name[idx])
-    #   
-    # })
+    idx <- sample(dim(lunch_spots)[1], 1) # pick random lunch spot
     
     output$map <- renderLeaflet({
        
@@ -22,13 +14,18 @@ function(input, output) {
       
         addProviderTiles(providers$Esri.NatGeoWorldMap, options = providerTileOptions(noWrap = TRUE)) %>%
         
-        addMarkers(lng = lunch_spots$lng[idx], lat = lunch_spots$lat[idx], label = lunch_spots$name[idx],
+        addMarkers(lng = lunch_spots$lng[idx], lat = lunch_spots$lat[idx],
+          
+          label = HTML(paste0("<font size=5 color=navy><b>", lunch_spots$name[idx], "</b></font>", "</br>",
+            "<font size=2 color=gray>", lunch_spots$address[idx], "</font>", "</br>",
+            "<font size=2 color=gray>", lunch_spots$city[idx], ", ", lunch_spots$state[idx], " ", lunch_spots$zipcode[idx], "</font>", "</br>")),
+          
           labelOptions = labelOptions(noHide = T,
                                       direction = "top",
-                                      style = list("color" = "MediumBlue",
-                                                   "font-size" = "18px",
-                                                   "box-shadow" = "5px 5px rgba(0,0,0,0.25)",
-                                                   "border-color" = "rgba(0,0,0,0.5)")))
+                                      style = list("box-shadow" = "5px 5px rgba(0,0,0,0.25)",
+                                                   "border-color" = "rgba(0,0,0,0.25)")))
+      
+      
       
     })
     
